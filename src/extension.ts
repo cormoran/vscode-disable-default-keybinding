a2bd3775894298d7bfd03d35ab0c901c389034ae
cormoran <cormoran707@gmail.com>
2022-12-10 19:57:12 +0900

    Fix command name and initialization

--- src/extension.ts
+++ src/extension.ts
@@ -17,65 +17,71 @@ import {
 import { openAndGetGlobalKeybindingsUri } from "./customKeybinding";
 import { disableDefaultKeybindings } from "./disableDefaultKeybinding";
 
+export const EXTENSION_NAME = "disable-default-keybinding";
+export const COMMAND_DISABLE_DEFAULT_KEYBINDINGS = `${EXTENSION_NAME}.disable-default-keybindings`;
+export const COMMAND_RESTORE_BACKUP = `${EXTENSION_NAME}.restore-keybindings-backup`;
+export const COMMAND_DELETE_BACKUP = `${EXTENSION_NAME}.delete-all-backups`;
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  const f = async () => {
+  const setup = async () => {
     await vscode.workspace.fs.createDirectory(context.globalStorageUri);
     const backupDir = path.join(
       context.globalStorageUri.fsPath,
       "keybindings_backup"
     );
-    context.subscriptions.push(
-      vscode.commands.registerCommand(
-        "disable-default-keybinding.delete-all-backups",
-        async () => {
-          deleteAllBackupFiles(backupDir);
-        }
-      )
-    );
-    context.subscriptions.push(
-      vscode.commands.registerCommand(
-        "disable-default-keybinding.restore-keybindings-backup",
-        async () => {
-          const globalKeybindingsUri = await openAndGetGlobalKeybindingsUri();
-          if (!globalKeybindingsUri) {
-            return;
-          }
-          const backups = await collectBackupFiles(backupDir);
-          const backup = await vscode.window.showQuickPick(backups, {
-            title: "Select version to restore",
-          });
-          if (backup) {
-            await restoreKeybindingBackup(
-              vscode.Uri.file(path.join(backupDir, backup)),
-              globalKeybindingsUri,
-              backupDir
-            );
-          }
-        }
-      )
-    );
-
-    context.subscriptions.push(
-      vscode.commands.registerCommand(
-        "disable-default-keybinding.disable-default-keybindings",
-        async () => {
-          const globalKeybindingsUri = await openAndGetGlobalKeybindingsUri();
-          if (!globalKeybindingsUri) {
-            return;
-          }
-          await backupKeybindingFile(globalKeybindingsUri, backupDir);
-          await disableDefaultKeybindings(
-            globalKeybindingsUri,
-            getExtensionsToPreserveKeybinding(),
-            getCommandsToPreserveKeybinding()
-          );
-        }
-      )
-    );
+    vscode.window.showInformationMessage("setup finished!");
+    return {
+      backupDir: backupDir,
+    };
   };
-  f();
+  const future = setup();
+  context.subscriptions.push(
+    vscode.commands.registerCommand(COMMAND_DELETE_BACKUP, async () => {
+      const setup = await future;
+      deleteAllBackupFiles(setup.backupDir);
+    })
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(COMMAND_RESTORE_BACKUP, async () => {
+      const setup = await future;
+      const globalKeybindingsUri = await openAndGetGlobalKeybindingsUri();
+      if (!globalKeybindingsUri) {
+        return;
+      }
+      const backups = await collectBackupFiles(setup.backupDir);
+      const backup = await vscode.window.showQuickPick(backups, {
+        title: "Select version to restore",
+      });
+      if (backup) {
+        await restoreKeybindingBackup(
+          vscode.Uri.file(path.join(setup.backupDir, backup)),
+          globalKeybindingsUri,
+          setup.backupDir
+        );
+      }
+    })
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      COMMAND_DISABLE_DEFAULT_KEYBINDINGS,
+      async () => {
+        const setup = await future;
+        const globalKeybindingsUri = await openAndGetGlobalKeybindingsUri();
+        if (!globalKeybindingsUri) {
+          return;
+        }
+        await backupKeybindingFile(globalKeybindingsUri, setup.backupDir);
+        await disableDefaultKeybindings(
+          globalKeybindingsUri,
+          getExtensionsToPreserveKeybinding(),
+          getCommandsToPreserveKeybinding()
+        );
+      }
+    )
+  );
 }
 
 // This method is called when your extension is deactivated
